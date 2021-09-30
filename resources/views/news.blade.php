3497766ef9bbf3e1a0763e79996847e9d357574e
davidebortoloni <davidebortoloni@gmail.com>
2021-09-30 23:11:00 +0200

    Header dinamico (bonus)

---
+++ resources/views/news.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.1/css/bootstrap.min.css' integrity='sha512-6KY5s6UI5J7SVYuZB4S/CZMyPylqyyNZco376NM2Z8Sb8OxEdp02e1jkKk/wZxIEmjQ6DRCEBhni+gpr9c4tvA==' crossorigin='anonymous' />
+    <title>News</title>
+</head>
+
+<body>
+    <div class="container">
+        <header class="py-3 text-center">
+            <h1 class="mb-4">News</h1>
+            <ul class="row list-unstyled justify-content-between">
+                @foreach($links as $link)
+                <li class="col"><a href="{{ route($link['view']) }}">{{ $link['text'] }}</a></li>
+                @endforeach
+            </ul>
+        </header>
+    </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,166 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    $data = [
+        'links' => [
+            [
+                'text' => 'Home',
+                'view' => 'home',
+                'active' => true,
+            ],
+            [
+                'text' => 'Services',
+                'view' => 'services',
+                'active' => false,
+            ],
+            [
+                'text' => 'About Us',
+                'view' => 'about_us',
+                'active' => false,
+            ],
+            [
+                'text' => 'News',
+                'view' => 'news',
+                'active' => false,
+            ],
+            [
+                'text' => 'Account',
+                'view' => 'account',
+                'active' => false,
+            ],
+        ],
+    ];
+    return view('home', $data);
+})->name('home');
+
+Route::get('/services', function () {
+    $data = [
+        'links' => [
+            [
+                'text' => 'Home',
+                'view' => 'home',
+                'active' => false,
+            ],
+            [
+                'text' => 'Services',
+                'view' => 'services',
+                'active' => true,
+            ],
+            [
+                'text' => 'About Us',
+                'view' => 'about_us',
+                'active' => false,
+            ],
+            [
+                'text' => 'News',
+                'view' => 'news',
+                'active' => false,
+            ],
+            [
+                'text' => 'Account',
+                'view' => 'account',
+                'active' => false,
+            ],
+        ],
+    ];
+    return view('services', $data);
+})->name('services');
+
+Route::get('/about_us', function () {
+    $data = [
+        'links' => [
+            [
+                'text' => 'Home',
+                'view' => 'home',
+                'active' => false,
+            ],
+            [
+                'text' => 'Services',
+                'view' => 'services',
+                'active' => false,
+            ],
+            [
+                'text' => 'About Us',
+                'view' => 'about_us',
+                'active' => true,
+            ],
+            [
+                'text' => 'News',
+                'view' => 'news',
+                'active' => false,
+            ],
+            [
+                'text' => 'Account',
+                'view' => 'account',
+                'active' => false,
+            ],
+        ],
+    ];
+    return view('about_us', $data);
+})->name('about_us');
+
+Route::get('/news', function () {
+    $data = [
+        'links' => [
+            [
+                'text' => 'Home',
+                'view' => 'home',
+                'active' => false,
+            ],
+            [
+                'text' => 'Services',
+                'view' => 'services',
+                'active' => false,
+            ],
+            [
+                'text' => 'About Us',
+                'view' => 'about_us',
+                'active' => false,
+            ],
+            [
+                'text' => 'News',
+                'view' => 'news',
+                'active' => true,
+            ],
+            [
+                'text' => 'Account',
+                'view' => 'account',
+                'active' => false,
+            ],
+        ],
+    ];
+    return view('news', $data);
+})->name('news');
+
+Route::get('/account', function () {
+    $data = [
+        'links' => [
+            [
+                'text' => 'Home',
+                'view' => 'home',
+                'active' => false,
+            ],
+            [
+                'text' => 'Services',
+                'view' => 'services',
+                'active' => false,
+            ],
+            [
+                'text' => 'About Us',
+                'view' => 'about_us',
+                'active' => false,
+            ],
+            [
+                'text' => 'News',
+                'view' => 'news',
+                'active' => false,
+            ],
+            [
+                'text' => 'Account',
+                'view' => 'account',
+                'active' => true,
+            ],
+        ],
+    ];
+    return view('account', $data);
+})->name('account');
